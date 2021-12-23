65833f09b9554226827cab5cd8631a6460c045f3
ayaadelhassan <ayaadelhassandarweesh@gmail.com>
2021-12-23 21:12:58 +0200

    skeletonization and edge detection

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from preprocessing import *
 import cv2
 import os
 
-input_dir = 'ACdata_base/5/'
+input_dir = 'ACdata_base/9/'
 dirs = os.listdir(input_dir)
 for idx,img_dir in enumerate(dirs):
     print("processing img "+str(img_dir))
     pre = img_dir.split('.')[0]
     img_dir = input_dir + img_dir
-
     img = cv2.imread(img_dir, cv2.IMREAD_GRAYSCALE)
     # shadowFreeImg = RemoveShadow(img,True)
     binarizedImg =  Binarize_Histogram(img,pre)
+    ####preprocessing for feature extraction:
+    ####skeletonization
+    skeletonized = Skeletonization(binarizedImg, pre)
+    ####edge detection:
+    edged = LaplacianEdge(binarizedImg, pre)
