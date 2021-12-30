43557241888c4b45b28540055f6704a7be8ea888
ayaadelhassan <ayaadelhassandarweesh@gmail.com>
2021-12-30 19:34:26 +0200

    f1 f2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from featuresextraction import *
 import cv2
 import os
 from sklearn import svm
-from sklearn.metrics import classification_report,accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 
 input_dir = 'ACdata_base/1/'
@@ -12,6 +12,7 @@ dirs = os.listdir(input_dir)
 hog_features = []
 hog_features2 = []
 HVSL_feature = []
+LVL_features = []
 labels = []
 for idx,img_dir in enumerate(dirs):
     print("processing img "+str(img_dir))
@@ -29,9 +30,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["1"])
 
 input_dir = 'ACdata_base/2/'
@@ -52,9 +55,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["2"])
 
 input_dir = 'ACdata_base/3/'
@@ -75,9 +80,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["3"])
 
 input_dir = 'ACdata_base/4/'
@@ -98,9 +105,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["4"])
 
 input_dir = 'ACdata_base/5/'
@@ -121,9 +130,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["5"])
 
 input_dir = 'ACdata_base/6/'
@@ -144,9 +155,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["6"])
 
 input_dir = 'ACdata_base/7/'
@@ -167,9 +180,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["7"])
 
 input_dir = 'ACdata_base/8/'
@@ -190,9 +205,11 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["8"])
 
 input_dir = 'ACdata_base/9/'
@@ -213,33 +230,34 @@ for idx,img_dir in enumerate(dirs):
     HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
     ToE_descriptor = getToE(edged,img,pre) #histogram
     ToS_descriptor = getToS(skeletonized,img,pre) #histogram
+    LVL_descriptor = getLVL(skeletonized,img,pre)
     hog_features.append(ToS_descriptor)
     hog_features2.append(ToE_descriptor)
-    HVSL_feature.append([HVSL_decriptor[0], HVSL_decriptor[1]])
+    HVSL_feature.append(HVSL_decriptor)
+    LVL_features.append(LVL_descriptor)
     labels.append(["9"])
 
 
-print(len(hog_features))
-print(len(hog_features2))
-print(len(HVSL_feature))
-print(len(labels))
-print(HVSL_feature)
 #######classifier
 
-clf = svm.SVC()
+# clf = svm.SVC()
+clf = DecisionTreeClassifier()
 hog_features = np.array(hog_features)
 labels = np.array(labels)    
-data_frame = np.hstack((labels,HVSL_feature))
+data_frame = np.hstack((labels, HVSL_feature))
 np.random.shuffle(data_frame)
 percentage = 80
 partition = int(len(hog_features)*percentage/100)
 
-x_train, x_test = data_frame[:partition,:-1],  data_frame[partition:,:-1]
-y_train, y_test = data_frame[:partition,-1:].ravel() , data_frame[partition:,-1:].ravel()
+x_train, x_test = data_frame[:partition,1:],  data_frame[partition:,1:]
+y_train, y_test = data_frame[:partition,0].ravel() , data_frame[partition:,0].ravel()
 
-clf.fit(x_train,y_train)
+tree = clf.fit(x_train,y_train) 
+# clf.fit(x_train,y_train)
 
 y_pred = clf.predict(x_test)
-print("Accuracy: "+str(accuracy_score(y_test, y_pred)))
-print('\n')
-print(classification_report(y_test, y_pred))
+# print("Accuracy: "+str(accuracy_score(y_test, y_pred)))
+# print('\n')
+# print(classification_report(y_test, y_pred))
+print("accuracy is:")
+print(sum(y_pred == y_test) / float(len(y_test)))
