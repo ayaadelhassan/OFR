5bb12fa7c7b60faba7b912b54c448d586c08bbcc
nadaabdelmaboud <nada5aled52@gmail.com>
2022-04-07 10:33:55 +0200

    update src

---
+++ src/diacritics_features.py
@@ -0,0 +1,108 @@
+import numpy as np
+from preprocessing import *
+from math import sin,cos,pi
+import cv2
+import matplotlib.pyplot as plt
+
+def Diacritics_Segmentation(binarized):
+    hist = np.sum(1-binarized/255,axis=1)
+    baseline = np.argmax(hist)
+
+    im_floodfill = binarized.copy()
+
+    hImg, wImg = binarized.shape
+    mask = np.zeros((hImg+2, wImg+2), np.uint8)
+    for col in range(1,binarized.shape[1]):
+        if binarized[baseline,col-1]==0 and binarized[baseline,col]==255:
+            #flood fill
+            cv2.floodFill(im_floodfill, mask, (col-1, baseline), 255)
+    
+    cv2.imwrite("floodfill.png",im_floodfill)
+
+    contours,_ = cv2.findContours(255-im_floodfill, cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_NONE)
+    # demo= np.zeros((hImg, wImg), np.uint8)
+    # cv2.drawContours(demo, contours, -1, 255, -1)
+    # cv2.imwrite("contours.png",demo)
+    shapes=[]
+    for i,contour in enumerate(contours):
+        x,y,w,h  = cv2.boundingRect(contour)
+        if w<=3 or h<=3:
+            continue
+        contourImg =255- binarized[y:y+h,x:x+w].copy()
+        shapes.append(contourImg)
+        cv2.imwrite(f"diacritics/temp_{i}.png",contourImg)
+    return shapes
+
+def extractFeatures_CPRP(shape):
+    CP=[]
+    PR=[]
+    whitePixels = np.array(np.where(shape == 255))
+    whitePixels = whitePixels.T
+    mid = len(whitePixels)//2
+   
+   
+    #centroid point
+    x0,y0 = whitePixels[mid][1], whitePixels[mid][0]
+    # print(x0,y0)
+    # print(whitePixels)
+
+    R = max([ np.sqrt((pixel[0]-y0)**2 + (pixel[1]-x0)**2) for pixel in whitePixels])
+    R = int(np.floor(R))
+
+
+    for gama in range(0,R):
+        Fgama =0
+        for theta in range(0,360):
+            rad = (theta/180)*pi
+            x= int(np.floor(gama*cos(rad)))
+            y= int(np.floor(gama*sin(rad)))
+            
+            x += x0
+            y=y0 - y
+            #print(x,y)
+            x = min(x,shape.shape[1]-1)
+            y = min(y,shape.shape[0]-1)
+
+            Fgama+=shape[y][x]
+        CP.append(Fgama)
+
+    for theta in range(0,360):
+        Ftheta =0
+        for gama in range(0,R):
+            rad = (theta/180)*pi
+            x= int(np.floor(gama*cos(rad)))
+            y= int(np.floor(gama*sin(rad)))
+            x+=x0
+            y=y0 - y
+            x = min(x,shape.shape[1]-1)
+            y = min(y,shape.shape[0]-1)
+            Ftheta+=shape[y][x]
+        PR.append(Ftheta)
+    
+
+    fig1 = plt.figure()
+    ax1 = plt.axes()
+
+    x = list(range(0,R))
+    ax1.plot(x, CP)
+    plt.savefig('CP.png')
+    # plt.show()
+    fig2 = plt.figure()
+    ax2 = plt.axes()
+    x = list(range(0,360))
+    ax2.plot(x, PR)
+    plt.savefig('PR.png')
+    # plt.show()
+
+    
+
+
+
+############################Test############################################
+# img = cv2.imread("0210.jpg", cv2.IMREAD_GRAYSCALE)
+# # shadowFreeImg = RemoveShadow(img,True)
+# binarizedImg =  Binarize_Histogram(img,"0210")
+# shapes = Diacritics_Segmentation(binarizedImg)
+# extractFeatures_CPRP(shapes[2])
+
+
---
+++ src/main.py
@@ -0,0 +1,109 @@
+import numpy as np
+from preprocessing import *
+from featuresextraction import *
+import cv2
+import os
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import VotingClassifier,RandomForestClassifier
+from scipy.stats import mode
+
+
+HVSL_feature = []
+LVL_features = []
+HPP_features = []
+TOS_features = []
+Stats_features = []
+TH_features = []
+labels = []
+feature_to_plot = []
+for i in range(1, 10):
+    input_dir = f'../ACdata_base/{i}/'
+    dirs = os.listdir(input_dir)
+    for idx,img_dir in enumerate(dirs):
+        #print("processing img "+str(img_dir))
+        pre = img_dir.split('.')[0]
+        img_dir = input_dir + img_dir
+        img_rgb = cv2.imread(img_dir)
+        img = cv2.imread(img_dir, cv2.IMREAD_GRAYSCALE)
+        binarizedImg =  Binarize_Histogram(img,pre)
+
+        ####preprocessing for feature extraction:
+        ####our skeletonization
+        skeletonized = Skeletonization(binarizedImg, pre)
+
+        ###their skeletonization
+        skeleton = 255 - skeletonize(1-binarizedImg/255)*255
+        ####edge detection:
+        edged = LaplacianEdge(binarizedImg, pre)
+        
+        LVL_descriptor = getLVL(skeletonized,img,pre)
+        HVSL_decriptor = getHVSL(edged,img_rgb,pre) #contains 2 features (V/H,(#of black pixels/((H/total)+(V/total))))
+        HPP_descriptor = getHPP(cropToText(binarizedImg),pre)
+        TOS_descriptor = getTOS(img)
+        Stats_descriptor = getStatsFeatures(binarizedImg)
+        #TH_descriptor = getThicknessHist(skeleton, binarizedImg)
+        HVSL_feature.append(HVSL_decriptor)
+        LVL_features.append(LVL_descriptor)
+        HPP_features.append(HPP_descriptor)
+        TOS_features.append(TOS_descriptor)
+        Stats_features.append(Stats_descriptor)
+        #TH_features.append(TH_descriptor)
+        labels.append([i])
+
+#######classifier
+labels = np.array(labels)    
+
+
+X_train, X_test, y_train, y_test = train_test_split(HVSL_feature, labels, test_size=0.3,random_state=10, stratify=labels)
+clf_HVSL = svm.SVC(probability=True)
+clf_HVSL.fit(X_train, y_train)
+predicted_HVSL = clf_HVSL.predict_proba(X_test)
+
+X_train, X_test, y_train, y_test = train_test_split(HPP_features, labels, test_size=0.3,random_state=10, stratify=labels)
+clf_HPP = svm.SVC(probability=True)
+clf_HPP.fit(X_train, y_train)
+predicted_HPP = clf_HPP.predict_proba(X_test)
+
+X_train, X_test, y_train, y_test = train_test_split(Stats_features, labels, test_size=0.3,random_state=10, stratify=labels)
+clf_Stats = MLPClassifier(alpha=1e-05, hidden_layer_sizes=(9,18),random_state=1,solver='lbfgs',max_iter=10000,)
+clf_Stats.fit(X_train,y_train)
+predicted_Stats = clf_Stats.predict_proba(X_test)
+
+X_train, X_test, y_train, y_test = train_test_split(TOS_features, labels, test_size=0.3,random_state=10, stratify=labels)
+clf_TOS = svm.SVC(probability=True) #10 is the best -> 92%
+clf_TOS.fit(X_train,y_train)
+# predicted_TOS = np.argmax(clf_TOS.predict_proba(X_test), axis=1) + 1
+predicted_TOS = clf_TOS.predict_proba(X_test)
+
+X_train, X_test, y_train, y_test = train_test_split(LVL_features, labels, test_size=0.3,random_state=10, stratify=labels)
+clf_LVL = svm.SVC(probability=True)
+clf_LVL.fit(X_train,y_train)
+predicted_LVL = clf_LVL.predict_proba(X_test)
+
+summation = predicted_HPP + predicted_HVSL + predicted_TOS + predicted_Stats + predicted_LVL
+# stacked = np.vstack((predicted_Stats,predicted_TOS,predicted_HPP, predicted_HVSL, predicted_LVL))
+predicted = np.argmax(summation, axis=1) + 1
+print(predicted.shape)
+print(y_test.shape)
+
+# predicted = mode().T
+# print(predicted_HVSL.shape)
+# print(predicted)
+# print(y_test.shape)
+# voting_clf = VotingClassifier(estimators=[('clf_HVSL', clf_HVSL), ('clf_Stats', clf_Stats)], voting='hard')
+# voting_clf.fit(X_train, y_train)
+# predicted = voting_clf.predict(X_test)
+
+# acc = accuracy_score(y_test, preds)
+# f1 = f1_score(y_test, preds, average='weighted')
+# print("Accuracy is: " + str(acc))
+# print("F1 Score is: " + str(f1))
+# predicted = clf.predict(X_test)
+print(
+    #f"Classification report for classifier {voting_clf}:\n"
+    f"{classification_report(y_test, predicted)}\n"
+)
